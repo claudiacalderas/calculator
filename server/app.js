6ef117c4b31e866afa0677bff91d7a2ccb775b23
Claudia Calderas <claudia.calderas@gmail.com>
2017-03-18 18:55:35 -0500

    + - * / functionality coded

--- server/app.js
+++ server/app.js
@@ -5,9 +5,32 @@ var port = 5000;
 
 app.use(express.static('server/public'));
 
-
 app.get('/',function(req,res) {
   res.sendFile(path.resolve('server/public/views/index.html'));
+});
+
+app.get("/data/:operand1/:operand2/:operator",function(req,res) {
+  console.log(req.params);
+  console.log(req.params.operand1,req.params.operator,req.params.operand2);
+  var operand1 = parseInt(req.params.operand1);
+  var operand2 = parseInt(req.params.operand2);
+  var result;
+  switch (req.params.operator) {
+    case "add":
+        result = operand1 + operand2;
+      break;
+    case "subtract":
+        result = operand1 - operand2;
+      break;
+    case "multiply":
+        result = operand1 * operand2;
+      break;
+    case "divide":
+        result = operand1 / operand2;
+      break;
+  }
+  console.log('Result is', result);
+  res.send({result: result});
 });
 
 app.listen(port);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
   console.log("client ready");
+
+  $('.operator').on('click', function() {
+    console.log('operator button clicked');
+
+    var operator = $(this).attr("id");
+    console.log(operator);
+    // get numbers
+    var operand1 = $('#operand1').val();
+    var operand2 = $('#operand2').val();
+    console.log(operand1,operand2);
+    // ajax get
+    $.ajax({
+            type: "GET",
+            url: "/data/" + operand1 + "/" + operand2 + "/" + operator,
+            success: function(responseFromServer) {
+              console.log(responseFromServer);
+              // show result
+              $('#result').empty();
+              $('#result'). append('<span>' + responseFromServer.result + '</span>');
+            }
+    });
+  });
+
+  $('#subtractButton').on('click', function() {
+    console.log('subtractButton clicked');
+  });
+
+  $('#multiplyButton').on('click', function() {
+    console.log('multiplyButton clicked');
+  });
+
+  $('#divideButton').on('click', function() {
+    console.log('divideButton clicked');
+  });
+
+  $('#clearButton').on('click', function() {
+    console.log('clearButton clicked');
+  });
+
+
 });
